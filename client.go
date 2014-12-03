ba57a325eb98f4d6815d47fd24b40bf5f6047c7d
MarcusBergholm <Marcusbergholm@hotmail.com>
2014-12-03 13:11:37 +0100

    new stuff

--- client.go
+++ client.go
@@ -6,28 +6,70 @@ import (
     "bufio"
     "log"
     "os"
+    "bytes"
 )
 
-
+// Main function for client. Connects to the server thro
+// dial. If connected to server, client will loop send and 
+// fetch to/from server.
 func main() {
+	// Dials the server.
 	conn, err := net.Dial("tcp", ":3333")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Connected")
+	fmt.Println("Connected") // DEBUG
+	// Creates a new input reader to get input from keyboard.
 	reader := bufio.NewReader(os.Stdin)
+	// Fetches the menu
+	fetch(conn)
+	//Sends and fetches to/from server.
 	for {	
-		fmt.Print("Enter text: ")
-	    text, _ := reader.ReadString('\n')
-
-		var a int
-		buff := make([]byte, 1024)
-		conn.Write([]byte(text))
-		a, err = conn.Read(buff)
-		if err != nil {
-			log.Fatal(err)
-		}
-		s := string(buff[:a])
-		fmt.Println(s)
+		send(conn, reader)
+		fetch(conn)
 	}
+}
+
+// Sends standard input to server.
+func send(conn net.Conn, reader *bufio.Reader) {
+	//fmt.Print("Enter text: ") // DEBUG
+	// Reads keyboard input until newline.
+    input, _ := reader.ReadString('\n')
+    input = input[:len(input)-1]
+	// Creates a new buffer based 	on input string.
+	buf := bytes.NewBufferString(input)
+	if buf.Len() == 10 || buf.Len() == 0 {
+		buf.Write(make([]byte, 1))
+	}
+	// Take the next 10 bytes and then sent it.
+	outputbuf := buf.Next(10)
+	// If outputbuf has less then 10 exit the loop
+	for len(outputbuf) == 10 || len(outputbuf) == 0  {
+		conn.Write(outputbuf)
+		outputbuf = buf.Next(10)
+	}
+	if len(outputbuf) == 10 {
+		conn.Write(outputbuf)
+		conn.Write(make([]byte, 1))
+	} else {
+		conn.Write(outputbuf)
+	}
+}
+
+// Fetches data from server. 10 bytes at the time.
+// Breaks the fetch if if encounter a newline.
+func fetch(conn net.Conn) {
+	var input string
+	inputLen := 10
+	for inputLen == 10 {
+		// Create new array to store 10 bytes of input
+		inputBuffer := make([]byte, 10)
+		inputLen, _ = conn.Read(inputBuffer)
+		// append the 10 bytes to the input string (return string) 
+		input = input + string(inputBuffer[:inputLen])
+		// If we encounter a newline (ASCII code for NL == 10)
+		// break the loop.  
+		//if inputBuffer[inputLen-1] == 10 {break}
+	}
+	fmt.Println(input)
 }
--- server.go
+++ server.go
@@ -4,17 +4,21 @@ import (
     "fmt"
     "net"
     "os"
+    "bytes"
 )
 
 const (
     CONN_HOST = "localhost"
     CONN_PORT = "3333"
     CONN_TYPE = "tcp"
+    TEMP_MENU = "Welcome to Bank\n(1) Withdraw\n(2) Deposit\n(3) Balance\n(4) Change language\n(5) Log off"
+    TEMP_MENU_SWE = "Välkommen till Bank\n(1) Uttag\n(2) Insättning\n(3) Saldo"
+    TEMP_LOGIN = "Welcome to Bank\nTo login enter your card nr:"
 )
 
 func main() {
     // Listen for incoming connections.
-    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+    l, err := net.Listen(CONN_TYPE,":"+CONN_PORT)
     if err != nil {
         fmt.Println("Error listening:", err.Error())
         os.Exit(1)
@@ -31,25 +35,124 @@ func main() {
 	        os.Exit(1)
 	    }
 	    // Handle connections in a new goroutine.
-	    go handleRequest(conn)
+	    go handleConnection(conn)
 	}
 }
 
-// Handles incoming requests.
-func handleRequest(conn net.Conn) {
-	fmt.Println("User connected")
+// Handles incoming requests for the connection conn.
+func handleConnection(conn net.Conn) {
+	fmt.Println("User connected") // DEBUG for server
+	login(conn)
 	for {
-		  // Make a buffer to hold incoming data.
-		  buf := make([]byte, 1024)
-		  var a int
-		  // Read the incoming connection into the buffer.
-		  a, err := conn.Read(buf)
-		  if err != nil {
-		    fmt.Println("Error reading:", err.Error())
+		  // Read the incoming connection 
+		  s, err := fetch(conn)
+		  if err != nil {return}
+		  // Send a response back to connection
+		  switch s {
+		  case "1": withdraw(conn)
+		  case "2": deposit(conn)
+		  case "3": balance(conn)
+		  case "4": changeLang(conn)
+		  case "5": err = logOff(conn)
+		  default: send(conn, "Incorrect input\n" + TEMP_MENU)
 		  }
-		  s := string(buf[:a])
-		  s = "You entered: " + s
-		  // Send a response back to person contacting us.
-		  conn.Write([]byte(s))
-		}
+		  if err != nil {return}
+	}
 }
+
+// Sends the string input to the client with only
+// 10 bytes at the time.
+func send(conn net.Conn, output string) {
+	// Creates a new buffer based on input string.
+	buf := bytes.NewBufferString(output)
+	if buf.Len() == 10 {
+		buf.Write(make([]byte, 1))
+	}
+	// Take the next 10 bytes and then sent it.
+	outputbuf := buf.Next(10)
+	// If outputbuf has less then 10 exit the loop
+	for len(outputbuf) == 10 {
+		conn.Write(outputbuf)
+		outputbuf = buf.Next(10)
+	}
+
+	if len(outputbuf) == 10 || len(outputbuf) == 0 {
+		conn.Write(outputbuf)
+		conn.Write(make([]byte, 1))
+	} else {
+		conn.Write(outputbuf)
+	}
+}
+
+// Fetch what the client is requesting.
+// Breaks the featch by encountering a newline in the input.
+func fetch(conn net.Conn) (input string, err error) {
+	inputLen := 10
+	for inputLen == 10 {
+		// Create new array to store 10 bytes of input
+		inputBuffer := make([]byte, 10)
+		inputLen, err = conn.Read(inputBuffer)
+		// If we do not have any connection with the client 
+		// disconnect the client
+		if err != nil {
+			fmt.Println("User disconnected") // Only prints in server.
+		    return "", err
+		}
+		// append the 10 bytes to the input string (return string) 
+		input = input + string(inputBuffer[:inputLen])
+		// If we encounter a newline (ASCII code for NL == 10)
+		// break the loop.  
+		//if inputBuffer[inputLen-1] == 10 {break}
+	}
+	return input, nil
+}
+
+// Sends login screen to client and promts it to enter cardnr
+// and password. If user enterd a valid cardnr and password 
+// the user logs on and will see the Banks menu
+func login(conn net.Conn) {
+	send(conn, TEMP_LOGIN)
+	cardNum, _ := fetch(conn)
+	fmt.Println(cardNum)
+	send(conn, "Pleace enter your password:")
+	password, _ := fetch(conn)
+	fmt.Println(password)
+	send(conn, TEMP_MENU)
+}
+
+func withdraw(conn net.Conn) {
+	send(conn, "Enter the amount you wish to withdraw")
+	withdrawAmount, _ := fetch(conn)
+	fmt.Println(withdrawAmount)
+	send(conn, "You succesfully took out: " + withdrawAmount + "\n " + TEMP_MENU)
+}
+func deposit(conn net.Conn) {
+	send(conn, "Enter the amount you wish to deposit")
+	depositAmount, _ := fetch(conn)
+	fmt.Println(depositAmount)
+	send(conn, "You succesfully entered: " + depositAmount + "\n" + TEMP_MENU)
+}
+func balance(conn net.Conn) {
+	send(conn, "You have no balance\n" + TEMP_MENU)
+}
+func changeLang(conn net.Conn) {}
+func logOff(conn net.Conn) (err error) {
+	return err
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
